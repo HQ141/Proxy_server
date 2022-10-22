6244a15cf291528198c3cd40e5be8d59db63057d
HQ141 <77571990+HQ141@users.noreply.github.com>
2022-10-22 17:10:55 +0500

    Update pserver.py

--- pserver.py
+++ pserver.py
@@ -1,11 +1,16 @@
 from email import header
 from http.client import HTTPConnection
 import imp
+from pickle import TRUE
 import socket
-import http.client
-import http.server
-
-hconn=http.client.HTTPConnection("google.com")
-hconn.request("GET","/")
-resp=hconn.getresponse()
-print(resp)
+import requests
+server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server.bind(('localhost',8889))
+server.listen(1)
+conn,addr=server.accept()
+while TRUE:
+    data=conn.recv(1024)
+    data=data.split("\n")
+    top_header=data[0].split(" ")
+    r=requests.request(top_header[0],top_header[1])
+    print(r)
