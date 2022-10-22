050c2f107b9786ece15d0814ef7d9dbe6c5e1808
HQ141 <77571990+HQ141@users.noreply.github.com>
2022-10-22 16:48:42 +0500

    Update pserver.py

--- pserver.py
+++ pserver.py
@@ -5,15 +5,7 @@ import socket
 import http.client
 import http.server
 
-server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-server.bind(('localhost',8889))
-server.listen(1)
-while True:
-    conn,addr=server.accept()
-    data=conn.recv(1024).decode()
-    print(data)
-    data=data.split('\n')
-    temp=data[0].split(' ')
-    method=temp[0]
-    req=temp[1]
-    test=socket.gethostbyname(req)
+hconn=http.client.HTTPConnection("google.com")
+hconn.request("GET","/")
+resp=hconn.getresponse()
+print(resp)
